1b851b732b2a9051c25d9ebd79d2ea0644f267d9
Samuel Gould <gould.samuel@outlook.com>
2026-03-16 12:32:32 -0400

    Update views.py

--- game/views.py
+++ game/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+from .models import Game
+
+def index(request):
+    return render(request, 'game/index.html')
+
+def game_list(request):
+    games = Game.objects.all()
+    return render(request, 'game/game_list.html', {'games': games})
