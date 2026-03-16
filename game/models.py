57d42601c8073fe96228ba8441f39ce663e404b6
Samuel Gould <gould.samuel@outlook.com>
2026-03-16 12:22:58 -0400

    WIP - added model

--- game/models.py
+++ game/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Game(models.Model):
+  title = models.CharField(max_length=100)
+  genre = models.CharField(max_length=50)
+  year = models.IntegerField(max_length=4)
+  rate = models.FloatField()
+  is_free = models.BooleanField(default=True)
+  
+  def __str__(self):
+    return f'{self.title} ({self.year})'
